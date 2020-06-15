dfe99d9184bb5b3019462aed0d63a1c4a0ae0615
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-06-15 15:22:24 -0300

    AuditModel para guardar a data de inserção e modificação

--- Projeto_Django/core/models.py
+++ Projeto_Django/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class AuditModel(models.Model):
+    created_on = models.DateField('Criado em', auto_now_add=True)
+    updated_on = models.DateField('Autalizado em', auto_now=True)
+
+    class Meta:
+        abstract=True

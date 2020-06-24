6a0a516638797094a9935b9d91b0c53dd5940501
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-06-24 17:05:31 -0300

    Colocando na pagina os dados de leitos, confirmados e obitos

--- Projeto_Django/dashboard/views.py
+++ Projeto_Django/dashboard/views.py
@@ -10,15 +10,22 @@ def site_view(request):
     GOOGLE_API_KEY = env.get('GOOGLE_API_KEY')
 
 
-    data_atualizacao = DataAtualizacao.objects.all()
+    data_atualizacao = DataAtualizacao.objects.all().last()
     dados_estado = DadosEstado.objects.all()
     casos_cidade = CasosCidade.objects.all()
     leitos = Leitos.objects.all()
-    casos_sexo = CasosSexo.objects.all()
+    casos_sexo = CasosSexo.objects.all().last()
     casos_faixa_etaria = CasosFaixaEtaria.objects.all()
     comorbidades = Comorbidades.objects.all()
-        
-    data = {
+    
+    size = len(dados_estado)
+    obitos_atual = dados_estado.last().obitos
+    obitos_novos = obitos_atual - dados_estado[size-2].obitos
+    
+    confirmados_atual = dados_estado.last().confirmados
+    confirmados_novos = confirmados_atual - dados_estado[size-2].confirmados
+
+    data_casos_cidades = {
         'data': [{
             'date':str(obj.data.strftime('%d-%m-%Y')),
             'confirmados':obj.confirmados,
@@ -26,7 +33,7 @@ def site_view(request):
         } for obj in dados_estado]
     }
 
-    data2 = {
+    data_leitos = {
         'data': [{
             'date':str(obj.data.strftime('%d-%m-%Y')),
             'capacidadeClinicos':obj.capacidade_clinicos,
@@ -44,12 +51,18 @@ def site_view(request):
         'google_api_key': GOOGLE_API_KEY,
         'data_atualizacao': data_atualizacao,
         'dados_estado': dados_estado,
+        'confirmados_atual': confirmados_atual,
+        'confirmados_novos': confirmados_novos,
+        'obitos_atual': obitos_atual,
+        'obitos_novos': obitos_novos,
         'casos_cidade': casos_cidade,
-        'data_casos_cidade': json.dumps(data),
+        'data_casos_cidade': json.dumps(data_casos_cidades),
         'leitos': leitos,
+        'data_leitos': json.dumps(data_leitos),
         'casos_sexo': casos_sexo,
         'casos_faixa_etaria': casos_faixa_etaria,
         'comorbidades': comorbidades,
+
     }
 
     return render(request, 'dashboard/index.html', context)

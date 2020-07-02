cb257fb5c8cdb6397420f8a67b57862d15251bbd
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-07-02 14:59:00 -0300

    Alterando novamente o caminho dos arquivos estaticos

--- Projeto_Django/mapa_covid/settings.py
+++ Projeto_Django/mapa_covid/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/core/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = env.get("DEFAULT_FROM_EMAIL")

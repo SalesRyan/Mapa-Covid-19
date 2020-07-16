e6f80ea1e6564ae176a8aa4006109f143b9d627e
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-07-16 19:05:25 -0300

    fix: configurando o bd defalut para sqlite

--- mapa_covid/settings.py
+++ mapa_covid/settings.py
@@ -84,9 +84,11 @@ WSGI_APPLICATION = 'mapa_covid.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 

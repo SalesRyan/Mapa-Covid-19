703dc364e8f586026e26d078da5c0dfbcd69bb0d
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-07-15 22:58:36 -0300

    fix: desfazendo os teste de tentativa de deploy

--- Projeto_Django/mapa_covid/settings.py
+++ Projeto_Django/mapa_covid/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import environ
 
+from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -31,8 +32,7 @@ SECRET_KEY = env.get("SECRET_KEY")
 DEBUG = env.get("DEBUG")
 
 ALLOWED_HOSTS = ['mapa-covid.herokuapp.com/','localhost']
-
-
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 # Application definition
 
 INSTALLED_APPS = [
@@ -128,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = env.get("DEFAULT_FROM_EMAIL")
 EMAIL_USE_TLS = True

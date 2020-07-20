cab8385395ffa885ed75dc21a06139f702515128
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-07-20 15:32:01 -0300

    feat: mudando o bd para postgres

--- mapa_covid/settings.py
+++ mapa_covid/settings.py
@@ -85,10 +85,18 @@ WSGI_APPLICATION = 'mapa_covid.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },'postgres_heroku': {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME_PRODUCAO'),
+            'USER': config('DB_USER_PRODUCAO'),
+            'PASSWORD': config('DB_PASSWORD_PRODUCAO'),
+            'HOST': config('DB_HOST_PRODUCAO'),
+            'PORT': config('DB_PORT_PRODUCAO'),
+    },
 }
 
 

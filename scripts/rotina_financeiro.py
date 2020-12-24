2f100ea327d86e013bd0ab7164bf332093b1fffc
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-12-24 02:36:54 -0300

    update: hora da rotina

--- scripts/rotina_financeiro.py
+++ scripts/rotina_financeiro.py
@@ -2,8 +2,7 @@ import schedule
 import time
 from scripts.agrupamentos.povoar_atualizar_financeiro import verificar
 
-# schedule.every().day.at("0:00").do(verificar)
-schedule.every().day.at("0:01").do(verificar)
+schedule.every().day.at("00:01").do(verificar)
 
 while True:
     schedule.run_pending()
